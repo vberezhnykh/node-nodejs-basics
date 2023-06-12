46692a4b965cec6c61ae7bfd1151dc4282c0dcb6
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 13:45:47 +0300

    feat: add solution to create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { existsSync } from "node:fs";
+import * as fs from "node:fs/promises";
+import * as url from "url";
+// const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = `${__dirname}/files/fresh.txt`;
+    const isFileExisted = existsSync(pathToFile);
+    if (isFileExisted) throw new Error("FS operation failed");
+    else fs.writeFile(pathToFile, "I am fresh and young");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await create();

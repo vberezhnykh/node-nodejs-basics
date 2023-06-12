1cb4b05e3bbc89cce88ca88f4e687c0e7b0bf5f3
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 15:30:00 +0300

    feat: add solution to list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { existsSync } from "node:fs";
+import * as url from "url";
+import * as fs from "node:fs/promises";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const pathToFolder = `${__dirname}/files`;
+  if (!existsSync(pathToFolder)) throw new Error("FS operation failed");
+  const files = await fs.readdir(pathToFolder);
+  for (const file of files) {
+    console.log(file);
+  }
 };
 
 await list();

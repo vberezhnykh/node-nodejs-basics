5d9c5920b6e2e821aa6a7869647debfa3fa4ec7a
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 16:38:39 +0300

    feat: add solution to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const input = createReadStream(
+    `${__dirname}/files/fileToCalculateHashFor.txt`
+  );
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();

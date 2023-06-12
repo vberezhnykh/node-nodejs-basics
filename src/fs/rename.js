97b1e207805330802f834478666298753addf78b
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 15:20:51 +0300

    feat: add solution to rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { existsSync } from "node:fs";
+import * as url from "url";
+import * as fs from "node:fs/promises";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = `${__dirname}/files/wrongFilename.txt`;
+  const newFileName = "properFilename.md";
+  const newPath = `${__dirname}/files/${newFileName}`;
+  if (existsSync(oldPath) && !existsSync(newPath)) {
+    fs.rename(oldPath, newPath);
+  } else throw new Error("FS operation failed");
 };
 
 await rename();

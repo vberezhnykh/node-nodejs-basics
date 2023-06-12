6693cbc5a29e92452c5840973a7698dd1e3f99ca
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 15:24:15 +0300

    feat: add solution to delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { existsSync } from "node:fs";
+import * as url from "url";
+import * as fs from "node:fs/promises";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  const pathToFile = `${__dirname}/files/fileToRemove.txt`;
+  if (existsSync(pathToFile)) {
+    fs.rm(pathToFile);
+  } else throw new Error("FS operation failed");
 };
 
 await remove();

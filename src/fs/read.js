b67084cab062dc1e885d26f23a79299040342b60
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-12 15:32:53 +0300

    feat: add solution to read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { existsSync } from "node:fs";
+import * as url from "url";
+import * as fs from "node:fs/promises";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const path = `${__dirname}/files/fileToRead.txt`;
+  if (!existsSync(path)) throw new Error("FS operation failed");
+  const content = await fs.readFile(path, { encoding: "utf-8" });
+  console.log(content);
 };
 
 await read();

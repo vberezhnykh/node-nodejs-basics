dc7efd771809fd6812fe9b29619e8d917a85148b
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-13 19:40:00 +0300

    feat: add solution to write stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import * as url from "url";
+import { createWriteStream } from "node:fs";
+import { stdin } from "process";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(`${__dirname}/files/fileToWrite.txt`);
+  stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
 
 await write();

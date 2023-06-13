d1575752462c93d5a11fc38a988e4b4c3f2774de
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-13 20:07:38 +0300

    feat: add solution to transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { stdin, stdout } from "node:process";
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(data, encoding, callback) {
+      callback(null, data.toString().split("").reverse().join(""));
+    },
+  });
+
+  stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();

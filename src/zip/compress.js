e205e431b5dc5ef166555af9582d429b23cd9b06
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-13 20:17:57 +0300

    feat: add solution to compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import * as url from "url";
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(`${__dirname}/files/fileToCompress.txt`);
+  const destination = createWriteStream(`${__dirname}/files/archive.gz`);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();

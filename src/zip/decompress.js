bdaab1c178e50d293d2cbcea1ed7f5ebc3f21897
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-06-13 20:38:02 +0300

    feat: add solution to unzip task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from "fs";
+import * as url from "url";
+import { createUnzip } from "zlib";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const source = createReadStream(`${__dirname}/files/archive.gz`);
+  const destination = createWriteStream(
+    `${__dirname}/files/fileToCompress.txt`
+  );
+  const unzip = createUnzip();
+  source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
